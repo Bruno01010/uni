5dcbefe8c22a93f84588e881e420f19647f0ba3a
Bruno01010 <davidshelpakov062@gmail.com>
2024-11-25 18:51:23 +0300

    Delete main/snake.py

--- main/snake.py
+++
@@ -1,5 +0,0 @@
-Home_work = 12
-Time = 1.5
-Course_name = 'Python'
-Time_one = Time / Home_work
-print('Курс:',Course_name + ','' всего задач:',str(Home_work) + ','' затрачено часов:',str(Time) + ','' среднее время выполнения',Time_one,'часа.')
