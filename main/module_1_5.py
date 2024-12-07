d08743109260c089e44bc57f1d2bb562e66a53f6
Bruno01010 <davidshelpakov062@gmail.com>
2024-12-07 15:15:01 +0300

    Delete main/module_1_5.py

--- main/module_1_5.py
+++
@@ -1,7 +0,0 @@
-immutable_var = 1, 2.5, "Moon", False 
-print(immutable_var)
-# immutable_var [1] = 3   кортеж не поддерживает обращение по элементу
-# print(immutable_var)  мы не можем добавить какой-то отдельно взятый элемент или изменить какой-то элемент внутри кортежа
-mutable_list = [1, 2.5, "Moon"]
-mutable_list [2] = "Sun"
-print(mutable_list)
